95284aab2833bb43dc7a71a1388fa7dd7b68955c
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-09-11 18:57:38 +0530

    initial ingestion script

--- src/ingestion/load_raw_data.py
+++ src/ingestion/load_raw_data.py
@@ -6,9 +6,23 @@ def load_csv(
     # schema
 ):
     df = pd.read_csv(file_path)
+    print("Shape:")
     print(df.shape)
+
+    print("\nColumns:")
+    print(df.columns.tolist())
+
+    print("\nFirst 5 rows:")
     print(df.head())
+
+    print("\nData types:")
     print(df.dtypes)
+
+    print("\nMissing values:")
+    print(df.isnull().sum())
+
+    print("\nDuplicate rows:")
+    print(df.duplicated().sum())
 
 
 
