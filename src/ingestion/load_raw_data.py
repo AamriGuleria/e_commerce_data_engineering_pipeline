d61322e8ee6869f2bfc7ca89dd5df5af7d8efae8
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-09-11 18:45:55 +0530

    using pandas for raw data csv loading

--- src/ingestion/load_raw_data.py
+++ src/ingestion/load_raw_data.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
-df = pd.read_csv("C:\Users\asus\e_commerce_data_engineering_pipeline\dataset\Brazilian E-Commerce Public Dataset by Olist.csv.csv")
+def load_csv(
+    file_path,
+    # table_name,
+    # schema
+):
+    df = pd.read_csv(file_path)
+    print(df.shape)
+    print(df.head())
+    print(df.dtypes)
 
-print(df.shape)
-print(df.head())
-print(df.dtypes)
+
+
+load_csv("dataset\Brazilian E-Commerce Public Dataset by Olist.csv")
+
+
+# python -m src.ingestion.load_raw_data
