6b063cef43fbf2a4ae4f0b947d79ba9e0f0f47e8
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-09-15 12:20:41 +0530

    alembic initialization

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -1,12 +1,17 @@
 from logging.config import fileConfig
+import os
+import sys
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
+
+from models import Base
+from models import Customers, Orders, Payments, Products, Seller
+
 config = context.config
 
 # Interpret the config file for Python logging.
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
